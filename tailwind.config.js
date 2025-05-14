e7c0fcd5ea539af401af9754672caa219bd69700
valencia94 <valencia942003@gmail.com>
2025-05-14 18:55:35 -0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        ikusi: "#4ac795",
+        cvdex: "#0060af"
+      }
+    }
+  },
+  plugins: []
+};
