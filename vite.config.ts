e33edc2d9189d8fbbecb020b645f00d685ec4256
valencia94 <valencia942003@gmail.com>
2025-05-15 02:19:46 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,62 +1,12 @@
-import { ValidateEnv } from '@julr/vite-plugin-validate-env';
-import basicSsl from '@vitejs/plugin-basic-ssl';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { defineConfig, UserConfig } from 'vite';
-import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
-import svgr from 'vite-plugin-svgr';
+import path from 'path';
 
-import { envSchema } from './env.schema';
-
-export const port = 5173;
-
-export default defineConfig(
-  (configEnv): UserConfig => ({
-    base: '/',
-    publicDir: './public',
-    server: {
-      port,
-      strictPort: true,
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, 'src'),
     },
-    plugins: [
-      react(),
-      basicSsl(),
-      chunkSplitPlugin({
-        strategy: 'all-in-one',
-        customSplitting: {
-          app: [/src\//],
-          vendors: [/node_modules\//],
-        },
-      }),
-      svgr(),
-      ValidateEnv({
-        validator: 'zod',
-        schema: envSchema,
-      }),
-    ],
-    css: {
-      modules: {
-        generateScopedName:
-          configEnv.mode === 'development'
-            ? '[local]-[hash:base64:5]'
-            : '[hash:base64:5]',
-      },
-    },
-    test: {
-      globals: true,
-      exclude: [
-        '**/.{idea,git,github,husky,run,storybook,cache,output,temp}/**',
-        '**/{dist,node_modules,playwright,coverage,storybook-static,test-results}/**',
-      ],
-      environment: 'happy-dom',
-      setupFiles: './src/tests/vitest.setup.ts',
-      passWithNoTests: true,
-      allowOnly: true,
-    },
-    resolve: {
-      alias: {
-        '~/': `${__dirname}/src/`,
-        '@mui/styled-engine': '@mui/styled-engine-sc',
-      },
-    },
-  })
-);
+  },
+});
