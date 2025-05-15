b30040022b6115760adae6cab9726ee85bc2f238
valencia94 <valencia942003@gmail.com>
2025-05-15 03:55:49 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,16 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  root: '.', // 🧠 explicitly state the root
-  publicDir: 'public', // 🧠 ensures Vite picks up public/index.html
+  root: '.',                      // Ensure root path
+  publicDir: 'public',            // Pick up index.html
   build: {
     outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: 'index.html', // ✅ tells Vite where the entry point is
-    },
+    emptyOutDir: true
   },
   resolve: {
     alias: {
-      '~': path.resolve(__dirname, 'src'),
-    },
+      '~': path.resolve(__dirname, 'src')
+    }
   },
-  plugins: [react()],
+  plugins: [react()]
 });
