1835f210629a7138b0b89142ff98e053ea9a6b34
valencia94 <valencia942003@gmail.com>
2025-05-17 20:07:57 -0500

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -8,7 +8,7 @@ export default function Login() {
   };
   return (
     <main className="min-h-screen flex flex-col items-center justify-center gap-6 font-sans">
-      <img src="/assets/ikusi-logo.png" className="w-32" />
+      <img src="ikusi-logo.png" className="w-32" />
       <h1 className="text-3xl font-semibold">Sign in to Acta Platform</h1>
       <form onSubmit={onSubmit} className="flex gap-2">
         <input placeholder="Email" className="border p-1" />
