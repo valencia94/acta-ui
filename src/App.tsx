45652b3d1e8e73829c78fb5469841146a3e2b2bb
valencia94 <valencia942003@gmail.com>
2025-05-17 23:53:22 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login      from './pages/Login';
-import Dashboard  from './pages/Dashboard';
+
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/"      element={<Dashboard />} />
+        <Route path="/" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
