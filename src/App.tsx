87c5a88fcabe4c5be40ab8345a3e2d00586a62c3
valencia94 <valencia942003@gmail.com>
2025-05-17 23:45:48 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Dashboard from './pages/Dashboard';
-import Login from './pages/Login';
+import Login      from './pages/Login';
+import Dashboard  from './pages/Dashboard';
 
 export default function App() {
-  const authed = !!localStorage.getItem('jwt');
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route
-          path="/"
-          element={authed ? <Dashboard /> : <Navigate to="/login" replace />}
-        />
+        <Route path="/"      element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
