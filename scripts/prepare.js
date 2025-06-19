61f28bc16ec88f8945dadc9154b31536a783285c
valencia94 <valencia942003@gmail.com>
2025-06-19 14:35:54 -0500

    Update prepare.js

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -1,6 +1,10 @@
 // scripts/prepare.js
 if (!process.env.CI) {
-  const { execSync } = require('child_process');
-  execSync('npx playwright install chromium', { stdio: 'inherit' });
-  execSync('npx husky install', { stdio: 'inherit' });
+  const { execSync } = require("child_process");
+
+  // Install the single browser we need for local dev
+  execSync("npx playwright install chromium", { stdio: "inherit" });
+
+  // Set up Husky git hooks
+  execSync("npx husky install", { stdio: "inherit" });
 }
