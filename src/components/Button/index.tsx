922b4984c16f5bf4d974a2929832791ea1b74b6a
valencia94 <valencia942003@gmail.com>
2025-06-23 17:07:18 -0500

    Update index.tsx

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,4 +1,5 @@
 import { Button as ButtonMUI } from "@mui/material";
+import { ReactNode } from "react";
 import { ButtonProps } from "./types";
 
 export const Button = ({ onClick, children, className }: ButtonProps) => (
