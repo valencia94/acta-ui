068b9d5b3804ba9e13af432b1d5efe9b00a42e4f
valencia94 <valencia942003@gmail.com>
2025-06-23 17:07:41 -0500

    Update types.ts

--- src/components/Button/types.ts
+++ src/components/Button/types.ts
@@ -1,10 +1,7 @@
-// src/components/Button/types.ts
 import { ReactNode } from "react";
 
 export interface ButtonProps {
   onClick?: () => void;
-  /** Tailwind / MUI classes forwarded from callers */
-  className?: string;
-  /** Inner JSX that the button will render */
   children?: ReactNode;
+  className?: string;
 }
