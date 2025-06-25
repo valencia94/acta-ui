e21fd4b5c01e2baf639aa84abd91f94cbfd4acce
valencia94 <valencia942003@gmail.com>
2025-06-25 17:43:35 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,14 @@ import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
 export default defineConfig({
-  root: '.',
+  plugins: [react()],
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html'), // use root index.html
+    },
   },
-  plugins: [react()],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
