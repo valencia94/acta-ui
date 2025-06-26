67c259b1f09c31aedc7ae2e3ae19f5e96f18bcee
valencia94 <valencia942003@gmail.com>
2025-06-26 03:04:57 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,20 @@
-import { Amplify } from 'aws-amplify';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Amplify } from 'aws-amplify';
 
 import awsmobile from './aws-exports';
-
-Amplify.configure(awsmobile);
-
-import '@aws-amplify/ui-react/styles.css';
-
 import App from './App';
 
+import '@aws-amplify/ui-react/styles.css';
+import './tailwind.css';
+import './styles/variables.css';
+
+// 🔐 Configure Amplify with environment-specific settings
+Amplify.configure(awsmobile);
+
+// 🚀 Render the app
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-import './tailwind.css';
-import './styles/variables.css';
