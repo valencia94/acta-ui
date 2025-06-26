894fbfacd14e2a370017f2250a6905f3609a0c15
valencia94 <valencia942003@gmail.com>
2025-06-26 05:25:35 -0500

    Create index.tsx

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,23 @@
+// src/components/Button/index.tsx
+import { Button as MUIButton } from '@mui/material';
+import { ButtonProps } from './types';
+
+export function Button({
+  onClick,
+  children,
+  className,
+  disabled,
+  ...rest
+}: ButtonProps) {
+  return (
+    <MUIButton
+      onClick={onClick}
+      className={className}
+      disabled={disabled}
+      variant="contained"
+      {...rest}
+    >
+      {children}
+    </MUIButton>
+  );
+}
