d19cae376f6015f8597bd4ab7248d25e0fcf4239
valencia94 <valencia942003@gmail.com>
2025-07-13 11:06:35 -0500

    Create theme.ts

---
+++ src/theme.ts
@@ -0,0 +1,11 @@
+import { extendTheme } from '@chakra-ui/theme-utils';
+
+const theme = extendTheme({
+  colors: {
+    'ikusi-green': '#006b54',
+    'ikusi-teal': '#0094a8',
+    'ikusi-dark': '#002e24',
+  },
+});
+
+export default theme;
