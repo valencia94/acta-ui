55f512029e4213bad597ee31bd313154ee2add60
valencia94 <valencia942003@gmail.com>
2025-08-01 01:52:29 -0500

    refactor: bundle Amplify config & remove legacy script

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,6 +1,4 @@
-// AWS Amplify Configuration for ACTA-UI
-
-const awsmobile = {
+export default {
   aws_project_region: "us-east-2",
   aws_cognito_region: "us-east-2",
   aws_user_pools_id: "us-east-2_FyHLtOhiY",
@@ -12,73 +10,17 @@ const awsmobile = {
     scope: ["email", "openid", "profile"],
     redirectSignIn: "https://d7t9x3j66yd8k.cloudfront.net/",
     redirectSignOut: "https://d7t9x3j66yd8k.cloudfront.net/login",
-    responseType: "code",
-  },
-  aws_cloud_logic_custom: [
-    {
-      name: "ActaAPI",
-      endpoint: "https://q2b9avfwv5.execute-api.us-east-2.amazonaws.com/prod",
-      region: "us-east-2",
-    },
-  ],
-  aws_user_files_s3_bucket: "projectplace-dv-2025-x9a7b",
-  aws_user_files_s3_bucket_region: "us-east-2",
-  Auth: {
-    identityPoolId: "us-east-2:xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
-    region: "us-east-2",
-    userPoolId: "us-east-2_xxxxxxxx",
-    userPoolWebClientId: "xxxxxxxxxxxxxxxxxxxxxxxxxx",
-    identityPoolRegion: "us-east-2",
-    authenticationFlowType: "USER_SRP_AUTH",
-    mandatorySignIn: true,
-    Cognito: {
-      userPoolId: "us-east-2_xxxxxxxx",
-      userPoolClientId: "xxxxxxxxxxxxxxxxxxxxxxxxxx",
-      identityPoolId: "us-east-2:xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
-      signUpVerificationMethod: "code",
-      loginWith: {
-        email: true,
-        phone: false,
-        username: false,
-        oauth: {
-          domain: "us-east-2-fyhltohiy.auth.us-east-2.amazoncognito.com",
-          scopes: ["email", "openid", "profile"],
-          redirectSignIn: ["https://d7t9x3j66yd8k.cloudfront.net/"],
-          redirectSignOut: ["https://d7t9x3j66yd8k.cloudfront.net/login"],
-          responseType: "code",
-        },
-      },
-      userAttributes: {
-        email: { required: true },
-      },
-      passwordFormat: {
-        minLength: 8,
-        requireLowercase: true,
-        requireUppercase: true,
-        requireNumbers: true,
-        requireSpecialCharacters: true,
-      },
-    },
+    responseType: "code"
   },
   API: {
     REST: {
       ActaAPI: {
         endpoint: "https://q2b9avfwv5.execute-api.us-east-2.amazonaws.com/prod",
-        region: "us-east-2",
-      },
-    },
+        region: "us-east-2"
+      }
+    }
   },
   Storage: {
-    S3: {
-      bucket: "projectplace-dv-2025-x9a7b",
-      region: "us-east-2",
-    },
-  },
+    S3: { bucket: "projectplace-dv-2025-x9a7b", region: "us-east-2" }
+  }
 };
-
-// Attach to window for legacy support
-if (typeof window !== 'undefined') {
-  window.awsmobile = awsmobile;
-}
-
-export default awsmobile;
